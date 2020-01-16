65ce7464ec5557d1afb465cead8435cac25631e7
pmj9911 <parthjardosh@gmail.com>
2020-01-17 00:03:36 +0530

    integrating cloud detection using k means

--- IsroBackend/views.py
+++ IsroBackend/views.py
@@ -1,3 +1,4 @@
+import os
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,9 +6,10 @@ from rest_framework.parsers import MultiPartParser,JSONParser
 from django.core.serializers.json import DjangoJSONEncoder
 import json
 import traceback
-
+from . import cloudDetection
 class CloudDetails(APIView):
     parser_classes = (JSONParser,)
+    
     '''
     Point (int,int)
     TIR1 count double
@@ -17,21 +19,8 @@ class CloudDetails(APIView):
     height string
     '''
     def get(self,request):
-        try:
-            jsonresp = {
-                "posx":  120,
-                "posy":  240,
-                "tir1Count": 581.00,
-                "cloudy": True,
-                "type":  "cumulonimbus",
-                "topTemp": '320 K',
-                "height":  "120 metres",
-            }
-            return Response(jsonresp, content_type='application/json')
-        except Exception as e:
-                traceback.print_exc()
-                print(e)
-                return HttpResponse(status=403)
+        return HttpResponse(status=403)
+
     def post(self,request):
         # try:
         print(eval(request.body.decode('ASCII')))
@@ -46,8 +35,14 @@ class CloudDetails(APIView):
         posy = l['posy']
         print(posx,"\t",posy,"################")
         jsonresp = {
-            'status' : 'Success',
-        }
+                "posx":  posx,
+                "posy":  posy,
+                "tir1Count": "581.00",
+                "cloudy": True,
+                "type":  "cumulonimbus",
+                "topTemp": '320 K',
+                "height":  "120 metres",
+            }
         return Response(jsonresp, content_type='application/json',status=200)
         # except Exception as e:
         #     traceback.print_exc()
@@ -63,10 +58,15 @@ class CloudPredictionDetails(APIView):
     '''
     def get(self,request):
         try:
+            print(os.path.dirname(__file__))
+            tif_name =  os.path.dirname(__file__) + "/3DIMG_07NOV2019_0000_L1C_SGP.tif"
+            image_name = 'C:/Users/parth jardosh/Desktop/Desktop/sih2020/DjangoServer/IsroDjangoBackend/IsroBackend/media/images/output.png'
+            cloudDetection.detection(tif_name,image_name)
             jsonresp = {
                 'time_slot' : '07-11-19 00:30 - 01:00',
                 'direction' : 'North East',
                 'speed' : '50 kpmh',
+                'fileName' : 'cloud1.jpg',
             }
             return Response(jsonresp, content_type='application/json')
         except Exception as e:
@@ -75,3 +75,5 @@ class CloudPredictionDetails(APIView):
                 return HttpResponse(status=403)
     def post(self,request):
         return HttpResponse(status=403)
+
+image_file = os.path.dirname(__file__) + "/3DIMG_07NOV2019_0000_L1C_SGP.tif"
